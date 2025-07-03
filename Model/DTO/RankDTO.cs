1a0ef822f98994691c91f76930c869fcab57f4e0
toobidu <dungtien3kk@gmail.com>
2025-07-03 14:09:48 +0700

    feature: add rank dto

---
+++ Model/DTO/RankDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1.Model.DTO;
+
+public class RankDTO
+{
+    public string Username { get; set; }
+    public int Score { get; set; }
+    public string TimeTaken { get; set; }
+
+    public RankDTO(string username, int score, TimeSpan timeTaken)
+    {
+        Username = username;
+        Score = score;
+        TimeTaken = $"{timeTaken.TotalSeconds:F2}s";
+    }
+}
