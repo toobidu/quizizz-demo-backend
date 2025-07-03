bc3bdba64838184b6e91d625a99e4af6349c7e53
toobidu <dungtien3kk@gmail.com>
2025-07-03 12:00:46 +0700

    feature: add User DTO

--- Model/DTO/UserDTO.cs
+++ Model/DTO/UserDTO.cs
@@ -2,5 +2,14 @@
 
 public class UserDTO
 {
-    
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string TypeAccount { get; set; }
+
+    public UserDTO(string username, string password, string typeAccount)
+    {
+        Username = username;
+        Password = password;
+        TypeAccount = typeAccount;
+    }
 }
