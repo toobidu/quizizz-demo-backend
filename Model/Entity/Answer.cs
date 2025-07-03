26e73ab1d1c52c273ff3027f4c948833bd835324
toobidu <dungtien3kk@gmail.com>
2025-07-03 16:01:57 +0700

    feature: add answer core entity

---
+++ Model/Entity/Answer.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1.Model.Entity;
+
+public class Answer
+{
+    public int Id { get; set; }
+    public int QuestionId { get; set; }
+    public string AnswerText { get; set; } = string.Empty;
+    public bool IsCorrect { get; set; }
+}
---
+++ Model/Entity/UserAnswer.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1.Model.Entity;
+
+public class UserAnswer
+{
+    public int UserId { get; set; }
+    public int RoomId { get; set; }
+    public int QuestionId { get; set; }
+    public int AnswerId { get; set; }
+    public bool IsCorrect { get; set; }
+    public TimeSpan TimeTaken { get; set; }
+}
