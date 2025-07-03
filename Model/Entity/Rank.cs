d527ef2bd9b0077eabcba80cdced285fdfb30955
toobidu <dungtien3kk@gmail.com>
2025-07-03 10:53:25 +0700

    feature: add rank core entity

---
+++ Model/Entity/Rank.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1.Model.Entity;
+
+public class Rank
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int TotalScore { get; set; }
+    public int GamesPlayed { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    
+    public User User { get; set; }
+}
