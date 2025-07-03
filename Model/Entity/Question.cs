2924721bfd2a814049fdbc0b547cd94a80dd34c4
toobidu <dungtien3kk@gmail.com>
2025-07-03 10:52:14 +0700

    feature: add question core entity

---
+++ Model/Entity/Question.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1.Model.Entity;
+
+public class Question
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string CorrectAnswer { get; set; } 
+
+    public string OptionA { get; set; }
+    public string OptionB { get; set; }
+    public string OptionC { get; set; }
+    public string OptionD { get; set; }
+    
+    public ICollection<Room> Rooms { get; set; }
+}
