d72f3ee2754de7d089aa6076acf5329ea4ce08fe
toobidu <dungtien3kk@gmail.com>
2025-07-03 14:08:58 +0700

    feature: add question DTOs

---
+++ Model/DTO/QuestionDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1.Model.DTO;
+
+public class QuestionDTO
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string OptionA { get; set; }
+    public string OptionB { get; set; }
+    public string OptionC { get; set; }
+    public string OptionD { get; set; }
+
+    public QuestionDTO(int id, string text, string optionA, string optionB, string optionC, string optionD)
+    {
+        Id = id;
+        Text = text;
+        OptionA = optionA;
+        OptionB = optionB;
+        OptionC = optionC;
+        OptionD = optionD;
+    }
+}
---
+++ Model/DTO/SubmitAnswerRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1.Model.DTO;
+
+public class SubmitAnswerRequest
+{
+    public int QuestionId { get; set; }
+    public string SelectedAnswer { get; set; }
+
+    public SubmitAnswerRequest(int questionId, string selectedAnswer)
+    {
+        QuestionId = questionId;
+        SelectedAnswer = selectedAnswer;
+    }
+
+    public bool ValidField()
+    {
+        return SelectedAnswer is "A" or "B" or "C" or "D";
+    }
+}
