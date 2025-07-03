61ad6c71dfd0b1e0039f0a40548b7dbae41e33af
toobidu <dungtien3kk@gmail.com>
2025-07-03 11:01:13 +0700

    feature: add register DTO

---
+++ Model/DTO/RegisterRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1.Model.DTO;
+
+public class RegisterRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+
+    public RegisterRequest(string username, string password, string email)
+    {
+        Username = username;
+        Password = password;
+        Email = email;
+    }
+
+    public bool ValidField()
+    {
+        return !string.IsNullOrWhiteSpace(Username) &&
+               !string.IsNullOrWhiteSpace(Password) &&
+               !string.IsNullOrWhiteSpace(Email);
+    }
+}
