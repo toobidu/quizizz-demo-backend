388e4d9b0ff762ac42795a311e9baaf05a9d1666
toobidu <dungtien3kk@gmail.com>
2025-07-03 14:07:07 +0700

    chore: add helper connection to database

---
+++ Data/DatabaseHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp1.Data;
+
+public static class DatabaseHelper
+{
+    private static string GetConnectionString()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json");
+
+        IConfiguration config = builder.Build();
+
+        return config.GetConnectionString("DefaultConnection");
+    }
+
+    public static SqlConnection GetConnection()
+    {
+        return new SqlConnection(GetConnectionString());
+    }
+}
