7436c1ae9fcc7ee904949c7e14cb31a26c99dc97
toobidu <dungtien3kk@gmail.com>
2025-07-03 10:51:37 +0700

    feature: add room core entity

---
+++ Model/Entity/Room.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1.Model.Entity;
+
+public class Room
+{
+    public int Id { get; set; }
+    public string RoomCode { get; set; }
+    public string RomeName { get; set; }
+    public bool IsPrivate { get; set; }
+    public int OwnerId { get; set; }
+    
+    public User Owner { get; set; }
+    public ICollection<RoomPlayer> Players { get; set; }
+    public ICollection<Question> Questions { get; set; }
+}
---
+++ Model/Entity/RoomPlayer.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1.Model.Entity;
+
+public class RoomPlayer
+{
+    public int RoomId { get; set; }
+    public int UserId { get; set; }
+    public int Score { get; set; }
+    public TimeSpan TimeTaken { get; set; }
+    
+    public Room Room { get; set; }
+    public User User { get; set; }
+}
