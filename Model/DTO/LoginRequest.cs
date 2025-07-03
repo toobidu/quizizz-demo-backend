c50bb4e5fabe18d0c147d05607fd7f28f2144fc7
toobidu <dungtien3kk@gmail.com>
2025-07-03 10:56:01 +0700

    feature: add login DTOs

---
+++ Model/DTO/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1.Model.DTO;
+
+public class LoginRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public LoginRequest(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+
+    public bool ValidField()
+    {
+        return !string.IsNullOrWhiteSpace(Username) &&
+               !string.IsNullOrWhiteSpace(Password);
+    }
+}
---
+++ Model/DTO/LoginResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1.Model.DTO;
+
+public class LoginResponse
+{
+    public string AccessToken { get; init; }
+    public string RefreshToken { get; init; }
+
+    public LoginResponse(string accessToken, string refreshToken)
+    {
+        AccessToken = accessToken;
+        RefreshToken = refreshToken;
+    }
+}
