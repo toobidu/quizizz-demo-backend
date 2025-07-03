5ebec79a867bdc21cde5c5d9e41a8e367f075478
toobidu <dungtien3kk@gmail.com>
2025-07-03 11:37:08 +0700

    feat: configuration system

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:9.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+
+COPY ConsoleApp1.csproj ./
+RUN dotnet restore "./ConsoleApp1.csproj"
+
+COPY . ./
+RUN dotnet build "./ConsoleApp1.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./ConsoleApp1.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "ConsoleApp1.dll"]
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int port = 8080;
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine($"Server is listening on http://localhost:{port}");
+
+        while (true)
+        {
+            using var client = listener.AcceptTcpClient();
+            using var stream = client.GetStream();
+
+            // Đọc request
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string requestText = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Request:");
+            Console.WriteLine(requestText);
+
+            // Chuẩn bị response
+            string responseBody = "hello";
+            string response =
+                "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/plain\r\n" +
+                $"Content-Length: {responseBody.Length}\r\n" +
+                "\r\n" +
+                responseBody;
+
+            // Gửi response
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
