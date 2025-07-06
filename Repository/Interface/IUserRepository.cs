334c44a536a60e0387d35fc959a9d09be3ce98c5
toobidu <dungtien3kk@gmail.com>
2025-07-06 17:21:53 +0700

    refactor: fix parameter in sql query

--- Repository/Interface/IUserRepository.cs
+++ Repository/Interface/IUserRepository.cs
@@ -4,11 +4,11 @@ namespace ConsoleApp1.Repository.Interface;
 
 public interface IUserRepository
 {
-    Task<User> GetByIdAsync(int id);
-    Task<User> GetByUsernameAsync(string username);
+    Task<User?> GetByIdAsync(int id);
+    Task<User?> GetByUsernameAsync(string username);
     Task<int> AddAsync(User user);
     Task UpdateAsync(User user);
-    Task DeleteAsync(int id);
+    Task<bool> DeleteAsync(int id);
     Task<bool> ExistsByUsernameAsync(string username);
     Task<IEnumerable<User>> GetAllAsync();
 }
